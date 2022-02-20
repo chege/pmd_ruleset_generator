4f79ce2d3594c089d27e7196b664049fb12bfb06
Christopher Egebakken <christopher.ekebakken@gmail.com>
2022-02-20 19:35:17 +0100

    init

---
+++ generate_ruleset.py
@@ -0,0 +1,115 @@
+import os.path
+import sys
+import tempfile
+from collections import namedtuple
+
+from git import Repo
+import xml.etree.ElementTree as ET
+import re
+
+import os
+
+
+class cd:
+    """Context manager for changing the current working directory"""
+
+    def __init__(self, new_path):
+        self.new_path = os.path.expanduser(new_path)
+
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
+
+
+RuleCategory = namedtuple('RuleCategory', 'name rules')
+
+
+def namespace(element):
+    m = re.match(r'{(.*)}', element.tag)
+    return m.group(1) if m else ''
+
+
+def git_rules(url, *category_files):
+    with tempfile.TemporaryDirectory() as tempdir:
+        Repo.clone_from(url, tempdir, depth=1, branch='pmd_releases/6.42.0')
+        result = []
+        with cd(tempdir):
+            for category_file in category_files:
+                tree = ET.parse(category_file)
+                ns = namespace(tree.getroot())
+                rules = tree.getroot().findall(f'{{{ns}}}rule')
+
+                result.append(
+                    RuleCategory(
+                        name=os.path.basename(category_file),
+                        rules=[rule.attrib['name'] for rule in rules]
+                    )
+                )
+
+            return result
+
+
+def clone_branch(url, to_path, branch_name, **kwargs):
+    Repo.clone_from(url, to_path, branch=branch_name, **kwargs)
+
+
+def category_path(path):
+    m = re.match(r'.*(category/.*)', path)
+    return m.group(1) if m else None
+
+
+def remove_all(tag_name, tree):
+    parent = {c: p for p in tree.iter() for c in p}
+    for rule in tree.getroot().findall(tag_name):
+        parent[rule].remove(rule)
+
+
+def rules(category_file):
+    tree = ET.parse(category_file)
+    ns = namespace(tree.getroot())
+    rules = tree.getroot().findall(f'{{{ns}}}rule')
+    return [rule.attrib['name'] for rule in rules]
+
+
+def main():
+    with tempfile.TemporaryDirectory() as tempdir:
+        clone_branch('https://github.com/pmd/pmd.git', tempdir, 'pmd_releases/6.42.0', depth=1)
+        files = [f'{tempdir}/{path}' for path in [
+            'pmd-java/src/main/resources/category/java/design.xml',
+            'pmd-java/src/main/resources/category/java/multithreading.xml',
+            'pmd-java/src/main/resources/category/java/bestpractices.xml',
+            'pmd-java/src/main/resources/category/java/documentation.xml',
+            'pmd-java/src/main/resources/category/java/errorprone.xml',
+            'pmd-java/src/main/resources/category/java/codestyle.xml',
+            'pmd-java/src/main/resources/category/java/performance.xml',
+            'pmd-java/src/main/resources/category/java/security.xml',
+        ]]
+
+        tree = create_output_tree(files[0])
+
+        ET.register_namespace('', namespace(tree.getroot()))
+
+        for file in files:
+            rule_elm = ET.SubElement(tree.getroot(), 'rule')
+            rule_elm.attrib['ref'] = category_path(file)
+
+            for rule in rules(file):
+                exclude_elm = ET.SubElement(rule_elm, 'exclude')
+                exclude_elm.attrib['name'] = rule
+
+        ET.indent(tree, '    ', 0)
+        tree.write('ruleset.xml')
+
+
+def create_output_tree(file):
+    tree = ET.parse(file)
+    rule_tag_name = f'{{{namespace(tree.getroot())}}}rule'
+    remove_all(rule_tag_name, tree)
+    return tree
+
+
+if __name__ == '__main__':
+    main()
